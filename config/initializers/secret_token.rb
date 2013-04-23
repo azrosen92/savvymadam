b53dc974b644e0fbb99dffab078371e52a698fc8
Aaron Rosen <aaronrosen@aaron-desktop.(none)>
2013-04-23 13:18:56 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AfricaJobSite::Application.config.secret_token = '0816bca1f22ff3f1577a4b10ea3b8f8ea818dc65a46af2fc31b8bd2135c9339f2ba0b3314076db531253d4037d8ac19bbc8f622e50d927f60ea71cb7e7c28584'
