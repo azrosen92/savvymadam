ebc2bad771c1b6df285f25fbf9ed76430475c46f
Aaron Rosen <azrosen92@gmail.com>
2013-05-06 20:20:46 -0400

    touch up home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-<<<<<<< HEAD
-
-=======
     @contact = Contact.new
->>>>>>> 04669b38d30ffafb794b52db49b5496c55da8cb3
   end
 
   def about
